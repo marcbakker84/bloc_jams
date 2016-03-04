2ed8da44f3f0cd6ea08feeae9547c9aad736b0ae
Marc Bakker <marc@burotijs.nl>
2016-03-04 13:08:08 +0100

    Checkpoint 23 assignment with some questions

---
+++ scripts/utilities.js
@@ -0,0 +1,11 @@
+var callBack = function(j) {
+  console.log("This is the output of the callback function vor point" + j)
+}
+
+var forEach = function(array, callback) {
+  for(i=0;i<array.length;i++) {
+    callBack(array[i]);
+  }
+};
+
+//why is there also 'callback' passed into the function??????
