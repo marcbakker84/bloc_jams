fbe32f5d5e11f74140ce6d748ef0364e8b866435
Marc Bakker <marc@burotijs.nl>
2016-03-04 11:08:19 +0100

    Checkpoint 23 added event handler to bloc jams

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,3 +1,14 @@
+var pointsArray = document.querySelectorAll('.point');
+
+var animatePoints = function (points) {
+    for(i=0;i<points.length;i++) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+};
+/*
 var animatePoints = function () {
 var points = document.querySelectorAll('.point');
     for(i=0;i<points.length;i++) {
@@ -6,7 +17,24 @@ var points = document.querySelectorAll('.point');
         points[i].style.msTransform = "scaleX(1) translateY(0)";
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-};
+};*/
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+
+     window.addEventListener("scroll", function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }     
+     });
+ }
+//What is document.documentElement.scrollTop or document.body.scrollTop
 
 /*
 var animatePoints = function () {
