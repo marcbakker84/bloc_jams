1a68c186aa3aef31dfee413e5652bb0d03f2a601
Marc Bakker <marc@burotijs.nl>
2016-03-11 09:42:01 +0100

    checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -40,31 +40,31 @@ window.onload = function() {
 
 var pointsArray = document.querySelectorAll('.point');
 
-var animatePoints = function (points) {
-    for(i=0;i<points.length;i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
 
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+$(window).load(function() {
+    if ($(window).height() > 950) {
+        animatePoints();
+}
 
+var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-     window.addEventListener("scroll", function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-             pointsArray.forEach(animatePoints);
-         }     
-     });
- }
+$(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+        animatePoints();
+        }
+    });
+ });
+
 
 //What is document.documentElement.scrollTop or document.body.scrollTop
 
